27899ee6a7fb108607e75a9e83bdb70030840713
Germano-Silva <germano.silva@ufn.edu.br>
2022-10-13 15:03:46 -0300

    Update ExerciciosStreamAPI.java

--- Dominando a Linguagem de Programação Java/CollectionsJava/CollectionsJava/src/collection/stream/ExerciciosStreamAPI.java
+++ Dominando a Linguagem de Programação Java/CollectionsJava/CollectionsJava/src/collection/stream/ExerciciosStreamAPI.java
@@ -10,35 +10,112 @@ public class ExerciciosStreamAPI {
     public static void main(String[] args) {
         List<String> numerosAleatorios =
                 Arrays.asList("1", "0", "4", "1", "2", "3", "9", "9", "6", "5");
-
-//        System.out.println("Imprima todos os elementos dessa lista de String: ");
-//        numerosAleatorios.stream()
-//                 .forEach(System.out::println);
-//        numerosAleatorios.forEach(System.out::println);
-
-//        System.out.println("Pegue os 5 primeiros números e coloque dentro de um Set:");
-//        numerosAleatorios.stream()
-//                .limit(5)
-//                .collect(Collectors.toSet())
-//                .forEach(System.out::println);
-//        Set<String> numerosAleatorios5Primeiros = numerosAleatorios.stream()
-//                .limit(5)
-//                .collect(Collectors.toSet());
-//        System.out.println(numerosAleatorios5Primeiros);
-
-//        System.out.println("Transforme esta lista de String em uma lista de números inteiros.");
-//        List<Integer> numerosAleatorios1 = numerosAleatorios.stream()
-//                .map(Integer::parseInt).collect(Collectors.toList());
-        List<Integer> numerosAleatoriosInteger = numerosAleatorios.stream()
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-
-        //.forEach(System.out::println);
-
+/**================================Utilizando o ForEach==========================*/
+        System.out.println("Imprima todos os elementos dessa lista de String: ");
+        List<Integer> numerosAleatoriosInteger = numerosAleatorios.stream().map(Integer::parseInt).collect(Collectors.toList());
+/**================================Método completo====================================
+ *       numerosAleatorios.stream().forEach(new Consumer<String>() {
+ *       📖 para utilizar o forEach é necessario utilizar uma interface funcional
+ *          @Override
+ *          public void accept(String s) {
+ *          📖 a utilização desse metodo vai pegar elemento por elemento do tipo String e vai imprimir um por um
+ *              System.out.println(s);
+ *            }
+ *          });
+ */
+/**================================Usuando a simplificação lambida=============================
+ *         📖 Usando o metodo de lambida simplificando
+ *         numerosAleatorios.stream().forEach(s -> System.out.println(s));
+ */
+/**================================Simplificando para método de referencia=============================
+ *          Usando o forEach com o metodo liste não é necessario usar o stream
+ *          Para maior simplificação devemos tranformar o lambida para um metodo de referencia
+ *          numerosAleatorios.forEach(System.out::println);
+ */
+/**================================Adicionando os 5 primeiros números em um set e imprimindo eles==================
+ *
+ *         System.out.println("Pegue os 5 primeiros números e coloque dentro de um Set:");
+ *         numerosAleatorios.stream()
+ *                 .limit(5)
+ *                 .collect(Collectors.toSet())
+ *                 .forEach(System.out::println);
+ */
+/**================================Adicionando os 5 primeiros número em um set=========================
+ *         Set<String> numerosAleatorios5Primeiros = numerosAleatorios.stream()
+ *                 .limit(5)
+ *                 .collect(Collectors.toSet());
+ *         System.out.println(numerosAleatorios5Primeiros);
+ */
+/**================================Método completo====================================
+ *         📖 Utilizando sem abstração com o método completo
+ *         System.out.println("Transforme esta lista de String em uma lista de números inteiros.");
+ *         numerosAleatorios.stream()
+ *                 .map(new Function<String, Interger>() {
+ *                     @Override
+ *                     public Integer apply(String s){
+ *                         return Integer.parseInt(s);
+ *                     }
+ *                 })
+ */
+/**================================Lambida=========================================
+ *
+ *         📖 utilizando a simplificação do lambida
+ *         numerosAleatorios.stream()
+ *                 .map(s -> Integer.parseInt(s);
+ */
+/**================================Método de Referencia=========================================
+ *
+ *        📖 Utilizando mais simplifiocação com referenciamento de metodo
+ *         numerosAleatorios.stream()
+ *                 .map(Integer::parseInt)
+ *                 .collect(Collectors.toList())
+ *                 .forEach(System.out::println);
+ */
+/**================================Armazenando em um Objeto====================================
+ *
+ *         📖 Simplificando para uma lista de inteiros utilizando um objetos para armazenar
+ *         List<Integer> numerosAleatorios1 = numerosAleatorios.stream()
+ *                 .map(Integer::parseInt)
+ *                .collect(Collectors.toList());
+ *
+ *
+ *         List<Integer> numerosAleatoriosInteger = numerosAleatorios.stream()
+ *                 .map(Integer::parseInt)
+ *                 .collect(Collectors.toList());
+ *
+ */
+/**================================Método completo===============================
+ *         System.out.println("Pegue os números pares e maiores que 2 e coloque em uma lista:");
+ *         📖 adicionado a um objeto o resultado
+ *         List<Integer> listParesMaioresQue2 = numerosAleatorios.stream()
+ *         📖 tranformando em interiro
+ *                 .map(Integer::parseInt)
+ *                 .filter(new Predicate<Integer>() {
+ *                     @Override
+ *                     public boolean test(Integer i) {
+ *                         if (i %2 ==0 && i>2)  return true;
+ *                         return false;
+ *                     }
+ *                 }).collect(Collectors.toList());
+ *         System.out.println(listParesMaioresQue2);
+ */
+/**================================Usuando a simplificação lambida=============================
+ *         System.out.println("Pegue os números pares e maiores que 2 e coloque em uma lista:");
+ *        📖 adicionado a um objeto o resultado
+ *         List<Integer> listParesMaioresQue2 = numerosAleatorios.stream()
+ *        📖 tranformando em interiro
+ *                 .map(Integer::parseInt)
+ *                 .filter(i -> (i %2 ==0 && i>2))
+ *                 .collect(Collectors.toList());
+ *         System.out.println(listParesMaioresQue2);
+ */
 
 //        System.out.println("Pegue os números pares e maiores que 2 e coloque em uma lista:");
 //        numerosAleatorios.stream()
+                    //tranformando em interiro
 //                .map(Integer::parseInt)
+                    //filtrando mumeros pares e maiores que 2
+
 //                .filter(i -> i % 2 == 0 && i > 2)
 //                .collect(Collectors.toList())
 //                .forEach(System.out::println);

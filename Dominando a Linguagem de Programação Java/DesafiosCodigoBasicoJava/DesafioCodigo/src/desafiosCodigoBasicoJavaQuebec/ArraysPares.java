66d1285ee0bbb1204dc1e29dafdd7220dbd2dcab
Germano-Silva <germano.silva@ufn.edu.br>
2022-10-14 11:30:31 -0300

    Desafios de Código Básico Java - Québec

---
+++ Dominando a Linguagem de Programação Java/DesafiosCodigoBasicoJava/DesafioCodigo/src/desafiosCodigoBasicoJavaQuebec/ArraysPares.java
@@ -0,0 +1,17 @@
+package desafiosCodigoBasicoJavaQuebec;
+/**================================"💻 Desafio"================================
+ * O objetivo deste desafio é resolvê-lo utilizando o seu conhecimento sobre Arrays,
+ * uma estrutura de dados que armazena uma coleção de dados em um bloco de memória.
+ * Dado uma array de valores {2, 3, 5, 7, 11, 13, 18, 34}, desenvolva um programa que
+ * retorne como resultado apenas os números pares dessa array.
+ *================================"Entrada"===============================
+ * A entrada consiste em um array de números inteiros positivos.
+ *================================"Saída"=================================
+ * A saída do desafio equivale aos números pares do Array, como mostra o exemplo a baixo:
+ *================================"Exemplo 1:"============================
+ *===========Entrada===============================Saída==================
+ *  2, 3, 5, 7, 11, 13, 18, 34  |                 2, 18, 34
+ */
+public class ArraysPares {
+
+}

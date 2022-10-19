4e8dbcaf34cb302fcf63b3c05af6a20b17bdab00
Germano-Silva <germano.silva@ufn.edu.br>
2022-10-19 12:24:29 -0300

    Update Carro.java

--- Dominando a Linguagem de Programação Java/ProgramacãoOrientadaObjetos/POO/src/estruturaBasicaPOO/Carro.java
+++ Dominando a Linguagem de Programação Java/ProgramacãoOrientadaObjetos/POO/src/estruturaBasicaPOO/Carro.java
@@ -1,18 +1,16 @@
 package estruturaBasicaPOO;
 
+/**================================✨ Boa pratica================================
+*Deve-se organizar o código segundo a ordem
+* Classe - Atributos - Construtor - get and set - Métodos
+*/
 //📖 Cria-se a Classe Carro
 public class Carro {
-    //📖 Cria-se os atributos da classe Carro
-    //📖 Filtra-se o tipo de atributo
+//📖 Cria-se os atributos da classe Carro
+//📖 Filtra-se o tipo de atributo
     String cor;
     String modelo;
     int capacidadeTanque;
-/**================================⚙ Método Construtor================================
- * 📖 Cria-se o Método conforme seu retorno
- * ⚙ Método Construtor: possibilita a criação de objetos a partir da Classe
- *     Carro(){}
- */
-
 /**================================💣 Método Destrutor================================
  * 💣 Método Destrutor: auxilia-se na destruição de um objeto,
  * deve sempre iniciar com a palavra "void" não possuindo retorno
@@ -34,18 +32,29 @@ public class Carro {
  * |=========================|
  */
 
-/**================================🟣 Método Set================================
- * Usamos set para definir valores. Esse tipo de método geralmente não retorna valores.
+/**================================⚙ Método Construtor================================
+ * 📖 Cria-se o Método conforme seu retorno
+ * ⚙ Método Construtor: possibilita a criação de objetos a partir da Classe
+ *     Carro(){}
  */
+    public Carro() {
+    }
+
+    public Carro(String cor, String modelo, int capacidadeTanque) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
 
 /**================================🟠 Método Get================================
  * Usamos get para obter informações. Esse tipo de método sempre retorna um valor.
  */
-
     public String getCor() {
         return cor;
     }
-
+/**================================🟣 Método Set================================
+ * Usamos set para definir valores. Esse tipo de método geralmente não retorna valores.
+ */
     public void setCor(String cor) {
         this.cor = cor;
     }
@@ -67,4 +76,7 @@ public class Carro {
     }
 
 
+    double totalValorTanque(double valorCombustivel){
+        return capacidadeTanque = (int) valorCombustivel;
+    }
 }

9f6db8822dcb5b616ad182509e8d13480a38bcce
Germano-Silva <germano.silva@ufn.edu.br>
2022-10-18 23:23:59 -0300

    Atualização apreendizagem orientação a  objeto

--- Dominando a Linguagem de Programação Java/ProgramacãoOrientadaObjetos/POO/src/estruturaBasicaPOO/Carro.java
+++ Dominando a Linguagem de Programação Java/ProgramacãoOrientadaObjetos/POO/src/estruturaBasicaPOO/Carro.java
@@ -1,5 +1,22 @@
 package estruturaBasicaPOO;
 
+//📖 Cria-se a Classe Carro
 public class Carro {
+    //📖 Cria-se os atributos da classe Carro
+    //📖 Filtra-se o tipo de atributo
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+    //📖 Cria-se o Método conforme seu retorno
+    //⚙ Método Construtor possibilita a criação de objetos a partir da Classe
+    Carro(){
 
+    }
+    //💣 Método Destrutor auxilia-se na destruição de um objeto,
+    // deve sempre iniciar com a palavra "void" não possuindo retorno
+    void finalise(){
+        //A sua função é a liberação de memora/recurso ocioso, dispensável e não mais utilizado
+    }
+
+    //🚛 Método Sobrecarga:
 }

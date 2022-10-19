f2331b7c7441d5807c37ffee0a1ad2a55acb4e3e
Germano-Silva <germano.silva@ufn.edu.br>
2022-10-19 12:56:18 -0300

    Create RodarAplicacao.java

---
+++ Dominando a Linguagem de Programação Java/ProgramacãoOrientadaObjetos/POO/src/estruturaBasicaPOO/RodarAplicacao.java
@@ -0,0 +1,8 @@
+package estruturaBasicaPOO;
+
+public class RodarAplicacao {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        carro1.setCor("AZUL");
+    }
+}

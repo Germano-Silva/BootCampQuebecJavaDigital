ace4d0157ef0f1beaff2d0e7e6abc67c4cb91389
Germano-Silva <germano.silva@ufn.edu.br>
2022-10-19 16:16:41 -0300

    Update RodarAplicacao.java

--- Dominando a Linguagem de Programação Java/ProgramacãoOrientadaObjetos/POO/src/estruturaBasicaPOO/RodarAplicacao.java
+++ Dominando a Linguagem de Programação Java/ProgramacãoOrientadaObjetos/POO/src/estruturaBasicaPOO/RodarAplicacao.java
@@ -4,5 +4,18 @@ public class RodarAplicacao {
     public static void main(String[] args) {
         Carro carro1 = new Carro();
         carro1.setCor("AZUL");
+        carro1.setModelo("BNW Série 3");
+        carro1.setCapacidadeTanque(59);
+        System.out.println(carro1.getModelo());
+        System.out.println(carro1.getCor());
+        System.out.println(carro1.getCapacidadeTanque());
+        System.out.println(carro1.totalValorTanque(6.39));
+
+        Carro carro2 = new Carro(" Cinza ", " Mercedes - Benz Classe C ", 66);
+
+        System.out.println(carro2.getModelo());
+        System.out.println(carro2.getCor());
+        System.out.println(carro2.getCapacidadeTanque());
+        System.out.println(carro2.totalValorTanque(6.46));
     }
 }

4c5913264fb5fc2ead44e256e80ed61d697d2bac
Germano-Silva <germano.silva@ufn.edu.br>
2022-10-25 12:46:20 -0300

    Update Robo.java

--- Dominando a Linguagem de Programação Java/DesafiosCodigoIntermediarioJava/DesafioCodIntermediario/src/desafio/Robo.java
+++ Dominando a Linguagem de Programação Java/DesafiosCodigoIntermediarioJava/DesafioCodIntermediario/src/desafio/Robo.java
@@ -16,25 +16,31 @@ import java.util.*;
 public class Robo{
     public static void main(String[] args){
         String movimentos = new Scanner(System.in).nextLine();
+
         var x = 0;
         var y = 0;
+
         // TODO: Implemente os movimentos do Robô e verifique sua posição:
-        for (int i = 0; i < movimentos.length(); i++){
+
+        for (int i = 0; i < movimentos.length(); i++) {
             char ch = movimentos.charAt(i);
-            if (ch == 'W'){
+            if(ch == 'W'){
                 x++;
-            }if (ch ==    ){
+            }if(ch == 'S'){
                 x--;
-            } if (ch ==    ){
+            }if(ch == 'D'){
                 y++;
-            }if (ch ==    ){
+            }
+            if(ch == 'A'){
                 y--;
             }
         }
+
         if (x == 0 && y == 0){
             System.out.print("true");
         }else{
             System.out.print("false");
         }
+
     }
 }

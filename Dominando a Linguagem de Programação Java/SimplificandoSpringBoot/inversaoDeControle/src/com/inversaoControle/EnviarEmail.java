c40b0cd0965016e2aa01129d79ac3a5bb778a8b8
Germano-Silva <germano.silva@ufn.edu.br>
2022-11-07 21:18:50 -0300

    atualizar

--- Dominando a Linguagem de Programação Java/SimplificandoSpringBoot/inversaoDeControle/src/com/inversaoControle/EnviarEmail.java
+++ Dominando a Linguagem de Programação Java/SimplificandoSpringBoot/inversaoDeControle/src/com/inversaoControle/EnviarEmail.java
@@ -3,6 +3,9 @@ package com.inversaoControle;
 public class EnviarEmail {
     public EnviarEmail(String tipo, String endereco, String senha){
     }
+    public static EnviarEmail obterDadosEviarEmail(){
+        return new EnviarEmail("Email", "germano@gmail.com", "123");
+    }
     public void retornar(String mensagem){
         System.out.println("Email enviado! ");
     }
--- Dominando a Linguagem de Programação Java/SimplificandoSpringBoot/inversaoDeControle/src/com/inversaoControle/Pedido.java
+++ Dominando a Linguagem de Programação Java/SimplificandoSpringBoot/inversaoDeControle/src/com/inversaoControle/Pedido.java
@@ -2,8 +2,12 @@ package com.inversaoControle;
 
 public class Pedido {
     public static void main(String[] args) {
-
+    Pedido pedido = new Pedido();
+    pedido.gravar();
     }
-    private EnviarEmail enviar;
-    public void grava
+    //classe dedicada a inversão de controle
+    private final EnviarEmail enviar = EnviarEmail.obterDadosEviarEmail();
+    public void gravar(){
+        this.enviar.retornar("Pedido Criado! ");
+    }
 }
